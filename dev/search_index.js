db73dcb62eadc45ebdd2606f9983c7f082e74c1b
Documenter.jl <documenter@juliadocs.github.io>
2022-06-13 10:27:54 +0000

    build based on 3fdd292

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Milvus","category":"page"},{"location":"#Milvus","page":"Home","title":"Milvus","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Milvus.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Milvus]","category":"page"}]
+}
